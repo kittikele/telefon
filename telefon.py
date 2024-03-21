eae9e6cb6251813e6f56e56bf995b090b12ac209
Kele Kitti <kelekitti06@gmail.com>
2024-03-21 20:13:00 +0100

    5-6. kesz

--- telefon.py
+++ telefon.py
@@ -40,7 +40,14 @@ ido = idopont.split(' ')
 while int(ido[0]) < 8 or int(ido[0]) > 12:
     idopont = input('Adjon meg egy időpontot! (óra perc másodperc)')
     ido = idopont.split(' ')
+eppen_hivasba_varakozok = []
+for i in range(len(munkaido_hivasok)):
+    if mpbe(int(ido[0]), int(ido[1]), int(ido[2])) > mpbe(int(munkaido_hivasok[i][0]), int(munkaido_hivasok[i][1]), int(munkaido_hivasok[i][2])) and mpbe(int(ido[0]), int(ido[1]), int(ido[2])) < mpbe(int(munkaido_hivasok[i][3]), int(munkaido_hivasok[i][4]), int(munkaido_hivasok[i][5])):
+        eppen_hivasba_varakozok.append(munkaido_hivasok[i])
+print(f'A várakozók száma: {len(eppen_hivasba_varakozok)-1} a beszélő a {eppen_hivasba_varakozok[0][6]}. hívó.')
 
+
+print('6. feladat')
 beszelok.append(munkaido_hivasok[0])
 for i in range(1, len(munkaido_hivasok)):
     
@@ -52,8 +59,5 @@ for i in range(1, len(munkaido_hivasok)):
         temp.append(elozo_hivas[5])
         beszelok.append(temp)
         
-print('6. feladat')
 print(f'Az utolsó telefonáló adatai a(z) {beszelok[-1][6]}. sorban vannak, {mpbe(int(beszelok[-1][7]), int(beszelok[-1][8]), int(beszelok[-1][9])) - mpbe(int(beszelok[-1][0]), int(beszelok[-1][1]), int(beszelok[-1][2]))} másodpercig várt.')
 
-print('temp', munkaido_hivasok)
-print('beszel', beszelok)
